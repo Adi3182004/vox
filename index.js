9d480bcdf459ab59b26e14b56f87390c3259b469
Adi3182004 <adi3182004@gmail.com>
2026-03-29 19:08:08 +0530

    final deployment ready

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
-const express = require('express');
-const path = require('path');
-const userRouter = require('./routes/user');
-const blogRouter = require('./routes/blog');
-const mongoose = require('mongoose');
-const cookieParser = require('cookie-parser');
-const { checkAuthenticationCookie } = require('./middlewares/authentication');
+const express = require("express");
+const path = require("path");
+const userRouter = require("./routes/user");
+const blogRouter = require("./routes/blog");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const { checkAuthenticationCookie } = require("./middlewares/authentication");
 const app = express();
-const PORT = 8000;
-const Blog = require('./models/blog');
 
-app.set('view engine', 'ejs');
+// FIX 1: Use dynamic PORT for deployment
+const PORT = process.env.PORT || 8000;
+
+const Blog = require("./models/blog");
+
+app.set("view engine", "ejs");
 
 // Middleware to parse the request body
 app.use(express.urlencoded({ extended: false }));
@@ -19,32 +22,40 @@ app.use(express.static(path.resolve("./public")));
 app.use(checkAuthenticationCookie("token"));
 
 // connect to the database
-mongoose.connect('mongodb://127.0.0.1:27017/blog-app')
-    .then(() => {
-        console.log('mongodb connection established');
-    })
-    .catch(err => console.log(err.message));
+mongoose
+  .connect(
+    "mongodb+srv://adi3182004:adi3182004@cluster0.ufwnhff.mongodb.net/blog-app?retryWrites=true&w=majority",
+  )
+  .then(() => {
+    console.log("mongodb connection established");
+  })
+  .catch((err) => console.log(err.message));
+
+// FIX 2: Root route (important for deployment)
+app.get("/", (req, res) => {
+  return res.redirect("/home");
+});
 
 // Home route
-app.get('/home', async (req, res) => {
-    const allBlogs = await Blog.find({}).sort({createdAt: -1});
-    return res.render('home', {
-        user: req.user,
-        blogs: allBlogs
-     });
-})
+app.get("/home", async (req, res) => {
+  const allBlogs = await Blog.find({}).sort({ createdAt: -1 });
+  return res.render("home", {
+    user: req.user,
+    blogs: allBlogs,
+  });
+});
 
 // user routes
-app.use('/user', userRouter);
+app.use("/user", userRouter);
 
 // blog routes
-app.use('/blog', blogRouter);
+app.use("/blog", blogRouter);
 
 // server configuration
-const server = app.listen(PORT, ()=> {
-    console.log(`listening on port ${PORT}`);
-})
+const server = app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
 
-server.on('error', (err)=> {
-    console.log(`error: ${err.message}`);
+server.on("error", (err) => {
+  console.log(`error: ${err.message}`);
 });
