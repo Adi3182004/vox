0894b688bc16c2f4023aa096c22f0ae5cac33b21
Adi3182004 <adi3182004@gmail.com>
2026-03-29 19:38:02 +0530

    fixed cloudinary upload

--- models/blog.js
+++ models/blog.js
@@ -1,26 +1,30 @@
-const { Schema, model } = require('mongoose');
-const User = require('./user');
+const express = require("express");
+const router = express.Router();
+const Blog = require("../models/blog");
+const upload = require("../middlewares/upload");
+const cloudinary = require("../config/cloudinary");
 
-const blogSchema = new Schema({
-    title : {
-        type: String,
-        required: true,
-    },
-    body : {
-        type: String,
-        required: true,
-    },
-    coverImage : {
-        type: String,
-    },
-    createdBy: {
-        type: Schema.Types.ObjectId,
-        ref: User,
+router.post("/", upload.single("coverImage"), async (req, res) => {
+  try {
+    let imageURL = "";
+
+    if (req.file) {
+      const result = await cloudinary.uploader.upload(req.file.path);
+      imageURL = result.secure_url;
     }
-},
-    {timestamps: true}
-);
 
-const Blog = model('Blog', blogSchema);
+    const blog = await Blog.create({
+      title: req.body.title,
+      body: req.body.body,
+      createdBy: req.user._id,
+      coverImage: imageURL,
+    });
 
-module.exports = Blog;
+    res.redirect("/home");
+  } catch (err) {
+    console.error("BLOG ERROR:", err);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+module.exports = router;
